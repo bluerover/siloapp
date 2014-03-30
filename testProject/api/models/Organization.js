3f5c3e8f19cd940ca0c85e38ba239c146e0dea27
andrewhassan <andrewihassan@gmail.com>
2014-03-30 13:03:44 -0400

    Added auth token to org

--- testProject/api/models/Organization.js
+++ testProject/api/models/Organization.js
@@ -13,8 +13,15 @@ module.exports = {
     name: {
       type: 'string',
       required: true
-    }
+    },
+    auth_token: 'string'
     
+  },
+
+  beforeCreate: function(attrs, next) {
+    var uuid = require('node-uuid');
+    attrs.auth_token = uuid.v4();
+    return next();
   }
 
 };
