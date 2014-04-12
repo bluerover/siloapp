5cdeb350087a65dbcb43b4517594a04c09eb8a9c
andrewhassan <andrewihassan@gmail.com>
2014-04-12 01:41:57 -0400

    Added db dump

--- testProject/config/bootstrap.js
+++ testProject/config/bootstrap.js
@@ -26,13 +26,13 @@ module.exports.bootstrap = function (cb) {
   sails.recent_alerts = {};
   sails.recent_rfid_data = {};
 
-  // createEventEmitters();
-  // setupTickEvent();
-  // setupBlueRoverApi();
-  // setupEventListeners();
-  // setupHandheldDataParser();
-  // loadRecentAlerts();
-  // loadRecentRfidData();
+  createEventEmitters();
+  setupTickEvent();
+  setupBlueRoverApi();
+  setupEventListeners();
+  setupHandheldDataParser();
+  loadRecentAlerts();
+  loadRecentRfidData();
 
   // DO NOT REMOVE! Without calling this callback, you will block the entire server
   cb();
