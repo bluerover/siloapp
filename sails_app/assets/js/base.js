dae98016bd806e590aa1ef71cc6d62c8b0c7276f
Jonathan Lam <jonathan.g.lam@gmail.com>
2014-08-29 13:47:21 -0400

    forgot base js

---
+++ sails_app/assets/js/base.js
@@ -0,0 +1,31 @@
+var typewatch = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  }  
+})();
+
+function addClickableRows() {
+  $(".clickableRow").click(function() {
+    window.document.location = $(this).data("url");
+  });  
+}
+
+$.extend($.expr[':'], {
+  'containsi': function(elem, i, match, array)
+  {
+    return (elem.textContent || elem.innerText || '').toLowerCase()
+    .indexOf((match[3] || "").toLowerCase()) >= 0;
+  }
+});
+
+String.prototype.format = String.prototype.f = function() {
+  var s = this,
+      i = arguments.length;
+
+  while (i--) {
+      s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+  }
+  return s;
+};
