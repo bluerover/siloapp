1f62671c5326ba07dfde70be75fd913417a0edbf
Mayank Gulati <mayank_gulati@hotmail.com>
2014-10-17 17:05:48 -0400

    workaround for making jlam an admin

--- sails_app/api/policies/isAdmin.js
+++ sails_app/api/policies/isAdmin.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller
-  if (req.session.authenticated && req.session.is_admin) {
+  if (req.session.authenticated && (req.session.is_admin || req.session.user == 1)) {
     return next();
   }
 
